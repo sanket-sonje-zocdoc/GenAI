c4266ac49d9cb54fd6955917405c8506ae7b0001
sanket-sonje-zocdoc <sanket.sonje@zocdoc.com>
2025-02-23 22:18:05 +0530

    Updated the recently updated mock files

--- Day1_PokemonApp/PokemonApp/Views/Mocks/PokemonCompareView+Mocks.swift
+++ Day1_PokemonApp/PokemonApp/Views/Mocks/PokemonCompareView+Mocks.swift
@@ -5,15 +5,12 @@
 //  Created by Sanket Sonje on 23/02/25.
 //
 
-extension PokemonListItem {
+class MockPokemonService: PokemonServiceAPI {
+    func fetchList<T>(offset: Int, limit: Int) async throws -> [T] where T : Decodable {
+        return [PokemonListItem.mockPokemonListItem, PokemonListItem.mockPokemonListItem] as! [T]
+    }
 
-    class MockPokemonService: PokemonServiceAPI {
-        func fetchList<T>(offset: Int, limit: Int) async throws -> [T] where T : Decodable {
-            return [PokemonListItem.mockPokemonListItem, PokemonListItem.mockPokemonListItem] as! [T]
-        }
-
-        func fetchItem<T>(url urlString: String) async throws -> T where T : Decodable {
-            return Pokemon.mockPokemon as! T
-        }
+    func fetchItem<T>(url urlString: String) async throws -> T where T : Decodable {
+        return Pokemon.mockPokemon as! T
     }
 }
